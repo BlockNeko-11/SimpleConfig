1eaa70599dd7b5ee18b7c508496b62e22279882b
Block-Mrlimr267 <block_mrlimr267@outlook.com>
2025-07-18 13:34:15 +0800

    撤回更改（1/1）

--- core/src/main/java/io/github/blockneko11/simpleconfig/api/holder/lang/EnumConfigHolder.java
+++
@@ -1,8 +0,0 @@
-package io.github.blockneko11.simpleconfig.api.holder.lang;
-
-import io.github.blockneko11.simpleconfig.api.holder.base.ObjectConfigHolder;
-
-public interface EnumConfigHolder<E extends Enum<E>> extends ObjectConfigHolder<E> {
-    interface Builder<E extends Enum<E>> extends ObjectConfigHolder.Builder<E> {
-    }
-}
